7fe2e4ad631f252c77098c7a4f2564d58e992b05
aleem-dev <aleem_shaikh786@yahoo.com>
2022-01-26 12:11:31 +0530

    completed repeat string and also written 1 test case

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeatation) {
+let text='';
+if(repeatation<0){
+    return 'ERROR';
+}
+for (let index = 0; index < repeatation; index++) {
+    text += str;
+}
+return text;
 };
 
 // Do not edit below this line
