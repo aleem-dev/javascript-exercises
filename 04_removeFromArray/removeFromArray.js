be83944d316e002107c6484a178e3b69ee1d1a0c
aleem-dev <aleem_shaikh786@yahoo.com>
2022-01-27 19:23:32 +0530

    excercise 4 remove from array complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, num) {
+  let arg= arguments;
+  let ar = arguments[0];
+    for (let i = 0; i < arg.length; i++) {
+        for (let index = 0; index < ar.length; index++) {
+            if (ar[index]===arguments[i+1]) {
+                //arr.splice(index,1);
+                ar.splice(index,1);
+                console.log(ar);   
+            }
+        }
+    }
+return ar;
 };
 
 // Do not edit below this line
