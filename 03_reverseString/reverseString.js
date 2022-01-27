3d3a8274284be2617961d7296b30603c28d55dff
aleem-dev <aleem_shaikh786@yahoo.com>
2022-01-27 15:59:41 +0530

    resolved reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+    let buff = '';
+    for (let index = str.length; index >= 1; index--) {
+    buff += str.slice((index-1),index);    
+        }
+  return buff;
 };
 
 // Do not edit below this line
