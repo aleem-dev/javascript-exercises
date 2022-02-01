3224a4b4a69de5f8f1225387a79e2e6d56750f81
aleem-dev <aleem_shaikh786@yahoo.com>
2022-02-01 15:57:57 +0530

    resolved exe 7, dont forget to write return statement at the end of function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  //let c = ((f-32)*(5/9)).toFixed(1);
+  let c = Math.round(((f-32)*(5/9))*10)/10;
+  console.log(`${f} f =  ${c} c`);
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+let f = Math.round(((c*(9/5))+32)*10)/10;
+console.log(`${c} c = ${f} f`);
+return f
 };
 
 // Do not edit below this line
