428a710505b1a036aacbb4024215642640c6a177
aleem-dev <aleem_shaikh786@yahoo.com>
2022-02-01 13:45:18 +0530

    resolved leap year with % and && operation

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if(num%4 === 0){
+        if(num%100 === 0){
+          if(num%100 === 0 && num%400 === 0){
+            console.log('leap year');
+            return true;
+          }else{
+          console.log('not a leap year');
+          return false;
+          }
+        }else{
+        console.log('leap year');
+        return true;
+        }
+      }
+      else{
+        console.log('not a leap year');
+        return false;
+      }
 };
 
 // Do not edit below this line
